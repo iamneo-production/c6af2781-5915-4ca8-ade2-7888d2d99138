602e2e6ff1dd38419d7b8d240bac8e66bd8b2a8b
deepan891 <npdeepan143@gmail.com>
2023-07-12 14:57:07 +0000

    Query Deepan

--- Voting_Deepan.sql
+++ Voting_Deepan.sql
@@ -1,24 +1,32 @@
-Select Year, Count(*) AS FemaleCandidateCount
+--one
+Select year,Count(*) AS FemaleCandidateCount
 from election
 where cand_sex='F'
 group by year;
+--end
 
+-two
 SELECT ST_NAME, YEAR, COUNT(*) AS TotalCandidates FROM ELECTION
 GROUP BY ST_NAME, YEAR;
+--end
 
+--three
 SELECT ST_NAME, SUM(TOTVOTPOLL) AS TotalVotes FROM ELECTION
 WHERE YEAR = 1987 AND PARTYNAME = 'BJP'
 GROUP BY ST_NAME;
+--end
 
+--four
 SELECT ST_NAME, COUNT(*) AS TotalCandidates
 FROM ELECTION
 WHERE YEAR = 2004
 GROUP BY ST_NAME;
+--end
 
+--five
 SELECT PARTYNAME, SUM(TOTVOTPOLL) AS TotalVotes
 FROM ELECTION
-WHERE YEAR = 2004 AND ST_NAME = 'UTTAR PRADESH'
+WHERE YEAR = 2014 AND ST_NAME = 'UTTAR PRADESH'
 GROUP BY PARTYNAME
-ORDER BY TotalVotes DESC
-LIMIT 5;
-
+ORDER BY TotalVotes DESC;
+--end
