7eb83e71fa0d1e6120f48695068e87dbed467750
deepan891 <npdeepan143@gmail.com>
2023-07-30 18:36:36 +0000

    commit

--- Deepan N P.sql
+++ Deepan N P.sql
@@ -1,30 +1,30 @@
 --one
-Select year,Count(*) AS FemaleCandidateCount
+Select year,Count(*) AS female_can_count_
 from election
 where cand_sex='F'
 group by year;
 --end
 
 -two
-SELECT ST_NAME, YEAR, COUNT(*) AS TotalCandidates FROM ELECTION
+SELECT ST_NAME, YEAR, COUNT(*) AS Tot_can_ FROM ELECTION
 GROUP BY ST_NAME, YEAR;
 --end
 
 --three
-SELECT ST_NAME, SUM(TOTVOTPOLL) AS TotalVotes FROM ELECTION
+SELECT ST_NAME, SUM(TOTVOTPOLL) AS Tot_Vot_ FROM ELECTION
 WHERE YEAR = 1987 AND PARTYNAME = 'BJP'
 GROUP BY ST_NAME;
 --end
 
 --four
-SELECT ST_NAME, COUNT(*) AS TotalCandidates
+SELECT ST_NAME, COUNT(*) AS Tot_Candi_
 FROM ELECTION
 WHERE YEAR = 2004
 GROUP BY ST_NAME;
 --end
 
 --five
-SELECT PARTYNAME, SUM(TOTVOTPOLL) AS TotalVotes
+SELECT PARTYNAME, SUM(TOTVOTPOLL) AS Tot_vot_
 FROM ELECTION
 WHERE YEAR = 2014 AND ST_NAME = 'UTTAR PRADESH'
 GROUP BY PARTYNAME
