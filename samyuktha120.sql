0820d88079adedb667a7e745672292368a576fa4
dandusirisha1010 <108048246+dandusirisha1010@users.noreply.github.com>
2023-07-31 23:15:53 +0530

    Update samyuktha120.sql

--- samyuktha120.sql
+++ samyuktha120.sql
@@ -1,23 +1,23 @@
 --1
-select year,count(*) as females from election
+select year,count(*) as fem__ from election
 where cand_sex='F' group by year;
 
 --2
-SELECT ST_NAME,YEAR,COUNT(*)AS numofcandi from ELECTION
+SELECT ST_NAME,YEAR,COUNT(*)AS num__of____ca from ELECTION
 group by st_name, year;
 
 --3
-SELECT ST_NAME,sum(totvotpoll) AS Tot_Votes FROM ELECTION
+SELECT ST_NAME,sum(totvotpoll) AS To___Vo FROM ELECTION
 WHERE YEAR=1987 AND PARTYNAME='BRS'
 GROUP BY ST_NAME;
 
 --4
-select st_name, count(*) as tot_candi from election
+select st_name, count(*) as to__can__ from election
 where year=2004
 group by st_name;
 
 --5
-select partyname,sum(totvotpoll) as tot_vot from ELECTION
+select partyname,sum(totvotpoll) as to___vo__ from ELECTION
 where year=2014 and st_name='TELANGANA'
 group by PARTYNAME
 order by total_vot DESC
