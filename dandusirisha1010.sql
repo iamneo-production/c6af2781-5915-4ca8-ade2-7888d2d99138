92039ffaa7c4d9cab7bd2eecce121b2ab28a91f7
dandusirisha1010 <108048246+dandusirisha1010@users.noreply.github.com>
2023-07-31 16:57:27 +0530

    Update dandusirisha1010.sql

--- dandusirisha1010.sql
+++ dandusirisha1010.sql
@@ -1,24 +1,37 @@
---1
-select year,count(*) as females from election
-where cand_sex='F' group by year;
+--ONE
+SELECT YEAR, COUNT(*) AS
+FemaleCandidateCount
+from election
+where cand_sex='F'
+group by year;
+--end
 
---2
-SELECT ST_NAME,YEAR,COUNT(*)AS numofcandi from ELECTION
-group by st_name, year;
+--two
+select ST_NAME, year, Count(*)
+as TotalCandidates from election
+group by ST_NAME, year;
+--end
 
---3
-SELECT ST_NAME,sum(totvotpoll) AS Tot_Votes FROM ELECTION
-WHERE YEAR=1987 AND PARTYNAME='BRS'
-GROUP BY ST_NAME;
+--three
+Select ST_NAME, sum(TOTVOTPOLL)
+as TotalVotes from election
+where year=1987 and PARTYNAME='BJP'
+group by ST_NAME;
+--end
 
---4
-select st_name, count(*) as tot_candi from election
+--four--
+select ST_NAME, count(*)
+as TotalCandidates
+from election
 where year=2004
-group by st_name;
+group by ST_NAME;
+--end
 
---5
-select partyname,sum(totvotpoll) as tot_vot from ELECTION
-where year=2014 and st_name='TELANGANA'
+--five --
+select PARTYNAME, sum(TOTVOTPOLL)
+as TotalVotes from election
+where year=2014 and ST_NAME='UTTAR PRADESH'
 group by PARTYNAME
-order by total_vot DESC
-FETCH first 5 rows only;
+order by TotalVotes desc
+fetch first 5 rows only;
+--end
